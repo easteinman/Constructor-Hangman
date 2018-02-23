b1d16c952101d75242781765e240db8e803be8c3
Eric <ericsteinman@me.com>
2018-02-23 13:14:11 -0600

    Started coding for index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+var prompt = require("prompt");
+var Word = require('./word.js');
+var userGuesses = 10;
+var zeldaCharacters = ["zelda","ruto","link","ganondorf","tingle","midna", "sheik", "epona", "saria", "urbosa", "zant", "navi"];
